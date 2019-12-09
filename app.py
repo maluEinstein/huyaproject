570af4c0a473b334da64a038373ee080eb3230ce
HASEE-PC\hasee <947809596@qq.com>
2019-12-09 09:40:18 +0800

    更新前端内容，添加后台和数据库交互内容

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from flask_cors import cross_origin
-from functools import reduce
+import pymysql
 
 app = Flask(__name__)
 result = {}
@@ -11,17 +11,67 @@ def hello_world():
     return render_template('upload.html')  # send_file('index.html')
 
 
-@app.route('/display', methods=['POST'])
+@app.route('/displayPYData', methods=['POST'])
 @cross_origin()
 def display():
+    result.clear()
     calData()
     return result
 
 
-@app.route('/display1', methods=['POST'])
+# 查询mysql里的数据
+@app.route('/displayMysqlDatabytime', methods=['POST'])
 @cross_origin()
-def display1():
-    pass
+def displayMysqlDatabytime():
+    result.clear()
+    gameName = []
+    gameHot = []
+    day = str(request.data).split("'")[1].split('&')[0].split('=')[1]
+    hour = str(request.data).split("'")[1].split('&')[1].split('=')[1]
+    sql = 'SELECT * FROM `room_hot_analsis` where day= "' + day + '" and hour=' + hour + ' ORDER BY `avg(room_hot)` LIMIT 100'
+    for i in toSQL(sql):
+        gameName.append(str(i[2]))
+        gameHot.append(str(i[3]))
+    result["gameName"] = gameName
+    result['gameHot'] = gameHot
+    print(result)
+    return result
+
+
+@app.route('/displayMysqlDatabytype', methods=['POST'])
+@cross_origin()
+def displayMysqlDatabytype():
+    result.clear()
+    gameHot = []
+    gametime = []
+    game_name = str(request.data).split("'")[1].split('=')[1]
+    print(game_name)
+    game_name="梦三国"
+    sql = 'SELECT * FROM `room_hot_analsis` where game_name="' + game_name + '" ORDER BY day,hour '
+    for i in toSQL(sql):
+        print(i[3])
+        gameHot.append(str(i[3]))
+        gametime.append(str(i[0]) + " " + str(i[1]))
+    result["gameHot"] = gameHot
+    result['gametime'] = gametime
+    return result
+
+
+@app.route('/displayMysqlDatabyroom', methods=['POST'])
+@cross_origin()
+def displayMysqlData2():
+    result.clear()
+    gameHot=[]
+    gametime=[]
+    print(str(request.data))
+    room_id=str(request.data).split("'")[1].split('=')[1]
+    sql='SELECT * FROM `basedata` WHERE room_id='+room_id+' ORDER BY day,hour'
+    for i in toSQL(sql):
+        gameHot.append(str(i[6]))
+        gametime.append(str(i[0]) + " " + str(i[1]))
+    result["gameHot"]=gameHot
+    result["gametime"]=gametime
+    return result
 
 
 @app.route('/upload', methods=['POST'])
@@ -30,12 +80,6 @@ def upload():
     f = request.files['file']
     f.save('newUpload.txt')
     return 'file uploaded success'
-
-
-# @app.route('/insertToDB', methods=['POST'])
-# @cross_origin()
-# def insertToDB():
-#     return 'insert success'
 
 
 def calData():
@@ -83,6 +127,14 @@ def calData():
     result["gameAve"] = gameAve
     result['gameHot'] = gameHot
     result['gameRoom'] = gameRoom
+
+
+def toSQL(sql):
+    conn = pymysql.connect(host='192.168.56.112', port=3306, user='hadoop', password='Hadoop@123', db='huya',
+                           charset='utf8')
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    return cursor.fetchall()
 
 
 if __name__ == '__main__':
--- test1.py
+++ test1.py
@@ -1,13 +1,26 @@
-import os
-days=os.listdir('C:/Users/hasee/Downloads/1')
-for day in days:
-    times = os.listdir('C:/Users/hasee/Downloads/1/'+day)
-    for i in times:
-        time=i.split('.txt')[0]
-        path = 'C:/Users/hasee/Downloads/1/' + day + '/' + time + '.txt'
-        print(path)
-        with open(path, 'r', encoding="UTF-8") as fp:
-            r = fp.readlines()
-        r = map(lambda x: str(day + '&' + time + '&' + x), r)
-        with open('C:/Users/hasee/Downloads/2/' + day + '.txt', 'a', encoding="UTF-8") as fw:
-            fw.writelines(list(r))
+# import os
+# days=os.listdir('C:/Users/hasee/Downloads/1')
+# for day in days:
+#     times = os.listdir('C:/Users/hasee/Downloads/1/'+day)
+#     for i in times:
+#         time=i.split('.txt')[0]
+#         path = 'C:/Users/hasee/Downloads/1/' + day + '/' + time + '.txt'
+#         print(path)
+#         with open(path, 'r', encoding="UTF-8") as fp:
+#             r = fp.readlines()
+#         r = map(lambda x: str(day + '&' + time + '&' + x), r)
+#         with open('C:/Users/hasee/Downloads/2/' + day + '.txt', 'a', encoding="UTF-8") as fw:
+#             fw.writelines(list(r))
+import pymysql
+#SELECT * FROM `room_hot_analsis` where day="2019-11-14" and hour=23 ORDER BY `avg(room_hot)` LIMIT 100
+day = "2019-11-14"
+hour = "23"
+conn = pymysql.connect(host='192.168.56.112', port=3306, user='hadoop', password= 'Hadoop@123', db='huya',
+                       charset='utf8')
+cursor = conn.cursor()
+# sql="SELECT * FROM room_hot_analsis where day = "  + day +  "AND hour=" + hour + " ORDER BY 'avg(room_hot)' LIMIT 100"
+sql = 'SELECT * FROM `room_hot_analsis` where day= "'+day+'" and hour='+hour+' ORDER BY `avg(room_hot)` LIMIT 100'
+# and hour= ' + hour + '
+res = cursor.execute(sql)
+print(cursor.fetchmany(10))
+conn.commit()
